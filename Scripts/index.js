53bc9e6549626b671620938af77d95998c56bef1
Jason <jasonahall1994@gmail.com>
2020-06-06 00:22:54 +0100

    intial commit

---
+++ Scripts/index.js
@@ -0,0 +1,336 @@
+//window.onload = function()
+//{
+	//console.log("working");
+//};
+
+var accident_prone = 
+{
+	question: "Name a Bad Job for Someone Who's Accident-Prone",
+	answers: 
+	[
+		"Driver", "Construction", "Police Officer", "Food Service",
+		"Glass Maker", "Window Washer", "Surgeon"
+	],
+	scores: [33, 20, 11, 10, 9, 7, 6]
+};
+
+ var no_touch = 
+{
+	question: "Name a Place You Visit Where You Aren't Allowed to Touch Anything",
+	answers: 
+	[
+		"Museum Gallery", "Zoo", "Gentleman's Club", "Antique Shop",
+		"Jewellery Store"
+	],
+	scores: [68, 20, 5, 18, 5]
+};
+
+ var wrestler_garment = 
+{
+	question: "Name a Garment You'd Probably Find in the Dressing Room for a Pro-Wrestling Event",
+	answers: 
+	[
+		"Tights", "Shorts", "Mask", "Robe",
+		"Belt", "Cape", "Leotard"
+	],
+	scores: [32, 17, 15, 9, 8, 8, 7]
+};
+
+ var hot_weather = 
+{
+	question: "Name a Recreational Activity Traditionally Done in Hot Weather",
+	answers: 
+	[
+		"Swimming", "Baseball", "Volleyball", "Surfing",
+		"Boating"
+	],
+	scores: [44, 18, 17, 11, 7]
+};
+
+var no_wash = 
+{
+	question: "Name an Article of Clothing You Wouldn't Usually Wash in the Washing Machine",
+	answers: 
+	[
+		"Shoe", "Bra", "Hat", "Coat",
+		"Sweater", "Suit", "Gown"
+	],
+	scores: [29, 21, 16, 13, 9, 7, 3] 
+};
+
+var catch_cold = 
+{
+	question: "Name a Public Place Where You're Likely to Catch a Cold or Flu Bug",
+	answers: 
+	[
+		"School", "Hospital", "Airplane", "Doctor's Surgery / Office",
+		"Public Transportation", "Shopping Centre", "Work"
+	],
+	scores: [41, 21, 10, 9, 6, 6, 5] 
+};
+
+var chased = 
+{
+	question: "Name Something People Are Often Chased by in Movies",
+	answers: 
+	[
+		"Monsters", "Cars", "Cops", "Bad Guys",
+		"Dogs"
+	],
+	scores: [37, 22, 19, 10, 10] 
+};
+
+var prince = 
+{
+	question: "If You Met the Real Prince Charming, How Would You Know it Was Him?",
+	answers: 
+	[
+		"Wears a Crown", "Rides a White Horse", "Good Looking", "Magical Kiss"
+	],
+	scores: [38, 25, 14, 12] 
+};
+
+var honeymoon = 
+{
+	question: "Name Something That a Man Had Better Not Take Along on His Honeymoon",
+	answers: 
+	[
+		"Another Woman", "Cellphone", "His Parents", "Work/Laptop",
+		"His Best friend", "Golf Clubs", "His Pet"
+	],
+	scores: [33, 17, 14, 12, 6, 5, 4] 
+};
+
+var waitress = 
+{
+	question: "Name Something a Customer Might Do to Annoy a Waitress",
+	answers: 
+	[
+		"Send Food Back", "Not Tip", "Flirt", "Snap Fingers",
+		"Spill Drinks"
+	],
+	scores: [40, 35, 6, 5, 4] 
+};
+
+var menus = 
+{
+	question: "Name a Language That You Often See on Restaurant Menus",
+	answers: 
+	[
+		"English", "French", "Italian", "Spanish",
+		"Chinese"
+	],
+	scores: [36, 22, 14, 11, 4] 
+};
+
+var no_bull = 
+{
+	question: "Name an Animal You Wouldn't Want in Your China Shop (Other Than a Bull)",
+	answers: 
+	[
+		"Elephant", "Cow", "Horse", "Bear",
+		"Hippo", "Rhino", "Moose"
+	],
+	scores: [47, 20, 10, 8, 4, 4, 3] 
+};
+
+var success = 
+{
+	question: "Name Something People Buy to Show They Are Successful",
+	answers: 
+	[
+		"Car", "House", "Jewellery", "Suit",
+		"Boat"
+	],
+	scores: [45, 41, 6, 4, 3] 
+};
+
+var boss = 
+{
+	question: "Name a Reason That Your Boss Would Give You a Raise (Other Than That You Work Hard)",
+	answers: 
+	[
+		"You're Dating Your Boss", "You're On Time", 
+		"You're Blackmailing Your Boss", "You've Flattered Your Boss"
+	],
+	scores: [54, 16, 12, 5] 
+};
+
+var under_bed = 
+{
+	question: "Name Something You Would Hate to Find Under Your Bed",
+	answers: 
+	[
+		"Monster", "Snake", "Food", "Spider", "Rodent", "Dust", 
+		"Person"
+	],
+	scores: [43, 16, 12, 10, 8, 4, 3] 
+};
+
+var rollercoaster = 
+{
+	question: "Name Something People Do While Riding a Rollercoaster",
+	answers: 
+	[
+		"Scream", "Hold Arms Up", "Get Sick", "Laugh", "Close Their Eyes"
+	],
+	scores: [43, 32, 13, 4, 4] 
+};
+
+var music = 
+{
+	question: "Name Something People Like to Do While Listening to Music",
+	answers: 
+	[
+		"Clean", "Exercise", "Study", "Garden", "Eat", "Dance", "Drive"
+	],
+	scores: [21, 18, 18, 12, 10, 8, 8] 
+};
+
+var colours = 
+{
+	question: "Name a Colour",
+	answers: 
+	[
+		"Blue", "Red", "Purple", "Green", "Yellow", "Pink"
+	],
+	scores: [50, 32, 17, 7, 5, 5] 
+};
+
+var transport = 
+{
+	question: "Name a Form of Transport",
+	answers: 
+	[
+		"Bus", "Car", "Train", "Bicycle", "Plane", "Bike"
+	],
+	scores: [46, 30, 19, 6, 4, 3] 
+};
+
+var dog = 
+{
+	question: "Name a Breed of Dog",
+	answers: 
+	[
+		"Labrador", "Pug", "Alsatian", "Poodle", "Collie", "Dalmation"
+	],
+	scores: [17, 17, 9, 9, 7, 6] 
+};
+
+var car = 
+{
+	question: "Name a Car Brand",
+	answers: 
+	[
+		"Ford", "Audi", "Volvo", "Mini", "Toyota", "BMW"
+	],
+	scores: [25, 13, 13, 10, 10, 6] 
+};
+
+var ball_sport = 
+{
+	question: "Name a Sport Played with a Ball",
+	answers: 
+	[
+		"Football", "Tennis", "Cricket", "Rugby", "Baseball", "Netball"
+	],
+	scores: [35, 22, 14, 11, 9, 7] 
+};
+
+var icecream = 
+{
+	question: "Name an Icecream Flavour",
+	answers: 
+	[
+		"Vanilla", "Chocolate", "Mint Choc Chip", 
+		"Pistachio", "Strawberry", "Raspberry Ripple"
+	],
+	scores: [36, 20, 20, 13, 8, 6] 
+};
+
+all_questions = 
+[
+	accident_prone, no_touch, wrestler_garment, hot_weather,
+	no_wash, catch_cold, chased, prince, honeymoon, waitress,
+	menus, no_bull, success, boss, under_bed, rollercoaster,
+	music, colours, transport, dog, car, ball_sport, icecream
+];
+
+var chosen = "";
+function generate_question()
+{
+	// reset the round points
+	document.getElementById("round_points").innerHTML = "0";
+	// Reset the Xs
+	x_count = 0;
+	
+	chosen = all_questions[Math.floor(Math.random() * all_questions.length)];
+	
+	//console.log(chosen.question);
+	//console.log(chosen.answers);
+	//console.log(chosen.scores);
+	
+	document.getElementById("question").innerHTML = chosen.question;
+	for(var i = 0; i < 8; i++)
+	{
+		var id = i + 1;
+		//console.log(id);
+		
+		// If already been removed in previous round, make a new button.
+			if(document.getElementById("b_" + id) == null)
+			{
+				// clear old texts
+				document.getElementById(id).innerHTML = "";
+				document.getElementById("s_" + id).innerHTML = "?";
+				
+				// make new button.
+				var new_button = document.createElement("button");
+				new_button.id = "b_" + id;
+				document.getElementById(id).appendChild(new_button);
+			}
+			
+		// if an answer is to be populated.
+		if(chosen.answers[i] != null)
+		{
+			document.getElementById("b_" + id).innerHTML = id;
+			document.getElementById("b_" + id).onclick = function(){showAnswer(this.innerHTML)};
+		}
+		else
+		{
+			document.getElementById("b_" + id).innerHTML = "-";
+		}
+	}
+	
+	
+};
+
+function showAnswer(id)
+{
+	document.getElementById("b_" + id).remove();
+	document.getElementById(id).innerHTML = chosen.answers[id-1];
+	document.getElementById("s_" + id).innerHTML = chosen.scores[id-1];
+	
+	document.getElementById("round_points").innerHTML = parseInt
+	(document.getElementById("round_points").innerHTML) + chosen.scores[id-1];
+};
+
+var x_count = 0;
+function incorrectAnswer()
+{
+	if(x_count < 3)
+	{
+		x_count++;
+		document.getElementById("x_" + x_count).style.visibility = "visible";
+	}
+}
+
+function clear_Xs()
+{
+	document.getElementById("x_1").style.visibility = "hidden";
+	document.getElementById("x_2").style.visibility = "hidden";
+	document.getElementById("x_3").style.visibility = "hidden";
+	
+	// Reset the Xs
+	x_count = 0;
+}
+	
